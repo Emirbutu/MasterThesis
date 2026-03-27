1b9556ce8f018709b6fb481491bdbcaf50b06d6e
Giuseppe M. Sarda <giuseppe.sarda16@gmail.com>
2026-03-27 13:42:38 +0000

    add tsmc28 tech setup file

---
+++ tsmc28_setup.tcl
@@ -0,0 +1,6 @@
+# Copyright 2024 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author: Giuseppe Sarda <giuseppe.sarda@esat.kuleuven.be>
+#         Emirhan Bututaki <emirhan.bututaki@student.kuleuven.be>
